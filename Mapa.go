f7d22c3c841477d9b1e88ff1026624e533c8d8d8
PolyakGG <separik8@gmail.com>
2024-02-09 22:02:34 +0400

    Mapa

---
+++ Mapa.go
@@ -0,0 +1,21 @@
+myMap := make(map[int]int) // Создаем пустой словарь (map).
+
+for i := 0; i < 10; i++ {
+	var val int
+	_, err := fmt.Scan(&val)
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+		continue // В случае ошибки продолжаем цикл.
+	}
+
+	// Проверяем наличие значения в map-е.
+	if result, exists := myMap[val]; exists {
+		// Если значение есть, выводим его.
+		fmt.Printf("%d ", result)
+	} else {
+		// Если значения нет, вызываем функцию work и записываем результат в map.
+		result := work(val)
+		myMap[val] = result
+		fmt.Printf("%d ", result)
+	}
+}
